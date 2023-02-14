014d6b89eb1142b5dbd40815369d2cd7a0a1fba5
Benoît CORTIER <bcortier@proton.me>
2023-02-14 00:37:27 -0500

    WIP

--- src/api/bread_world.rs
+++ src/api/bread_world.rs
@@ -1,8 +1,10 @@
 use anyhow::Context as _;
-use axum::extract::{Path, State};
+use axum::extract::State;
 use axum::routing::{delete, get, patch, post};
 use axum::{Json, Router};
+use axum_extra::extract::Query;
 use bread_world_models::Ingredient;
+use serde::Deserialize;
 use ulid::Ulid;
 
 use crate::api::ApiError;
@@ -11,57 +13,20 @@ use crate::AppState;
 pub fn make_router(state: AppState) -> Router {
     trace!("Make bread-world router");
     Router::new()
-        .route("/ingredients", get(list_ingredients))
-        .route("/ingredients", patch(patch_ingredient))
         .route("/ingredients", post(post_ingredient))
-        .route("/ingredients/:id", get(get_ingredient))
-        .route("/ingredients/:id", delete(delete_ingredient))
+        .route("/ingredients", get(get_ingredients))
+        .route("/ingredients", patch(patch_ingredient))
+        .route("/ingredients", delete(delete_ingredients))
+        .route("/ingredients/all", get(get_all_ingredients))
         .with_state(state)
 }
 
-pub async fn list_ingredients(State(s): State<AppState>) -> Result<Json<Vec<Ingredient>>, ApiError> {
-    let tree = s.db.open_tree("ingredients")?;
-    let ingredients = tree
-        .iter()
-        .map(|elem| {
-            let val = elem?.1;
-            let val = bincode::deserialize(&val)?;
-            Ok(val)
-        })
-        .collect::<Result<Vec<_>, ApiError>>()?;
-
-    Ok(Json(ingredients))
+#[derive(Deserialize)]
+struct ListQuery {
+    id: Vec<Ulid>,
 }
 
-pub async fn get_ingredient(Path(id): Path<Ulid>, State(s): State<AppState>) -> Result<Json<Ingredient>, ApiError> {
-    let key = u128::from(id).to_be_bytes();
-
-    let tree = s.db.open_tree("ingredients")?;
-    let value = tree
-        .get(key)?
-        .context("Ingredient not found")
-        .map_err(ApiError::not_found)?;
-
-    let ingredient: Ingredient = bincode::deserialize(&value)?;
-
-    Ok(Json(ingredient))
-}
-
-pub async fn delete_ingredient(Path(id): Path<Ulid>, State(s): State<AppState>) -> Result<Json<Ingredient>, ApiError> {
-    let key = u128::from(id).to_be_bytes();
-
-    let tree = s.db.open_tree("ingredients")?;
-    let value = tree
-        .remove(key)?
-        .context("Ingredient not found")
-        .map_err(ApiError::not_found)?;
-
-    let ingredient: Ingredient = bincode::deserialize(&value)?;
-
-    Ok(Json(ingredient))
-}
-
-pub async fn post_ingredient(State(s): State<AppState>, Json(ingredient): Json<Ingredient>) -> Result<(), ApiError> {
+async fn post_ingredient(State(s): State<AppState>, Json(ingredient): Json<Ingredient>) -> Result<(), ApiError> {
     let key = u128::from(ingredient.id).to_be_bytes();
     let value = bincode::serialize(&ingredient)?;
 
@@ -78,7 +43,32 @@ pub async fn post_ingredient(State(s): State<AppState>, Json(ingredient): Json<I
     Ok(())
 }
 
-pub async fn patch_ingredient(State(s): State<AppState>, Json(ingredient): Json<Ingredient>) -> Result<(), ApiError> {
+async fn get_ingredients(
+    Query(query): Query<ListQuery>,
+    State(s): State<AppState>,
+) -> Result<Json<Vec<Ingredient>>, ApiError> {
+    let tree = s.db.open_tree("ingredients")?;
+
+    let ingredients = query
+        .id
+        .into_iter()
+        .map(|id| {
+            let key = u128::from(id).to_be_bytes();
+            tree.get(key)?
+                .with_context(|| format!("{} not found", id))
+                .map_err(ApiError::not_found)
+        })
+        .map(|val| {
+            let val = val?;
+            let val = bincode::deserialize(&val)?;
+            Ok(val)
+        })
+        .collect::<Result<Vec<_>, ApiError>>()?;
+
+    Ok(Json(ingredients))
+}
+
+async fn patch_ingredient(State(s): State<AppState>, Json(ingredient): Json<Ingredient>) -> Result<(), ApiError> {
     let key = u128::from(ingredient.id).to_be_bytes();
     let value = bincode::serialize(&ingredient)?;
 
@@ -86,4 +76,44 @@ pub async fn patch_ingredient(State(s): State<AppState>, Json(ingredient): Json<
     tree.insert(key, value)?;
 
     Ok(())
+}
+
+async fn delete_ingredients(
+    Query(query): Query<ListQuery>,
+    State(s): State<AppState>,
+) -> Result<Json<Vec<Ingredient>>, ApiError> {
+    let tree = s.db.open_tree("ingredients")?;
+
+    let ingredients = query
+        .id
+        .into_iter()
+        .map(|id| {
+            let key = u128::from(id).to_be_bytes();
+            tree.remove(key)?
+                .with_context(|| format!("{} not found", id))
+                .map_err(ApiError::not_found)
+        })
+        .map(|val| {
+            let val = val?;
+            let val = bincode::deserialize(&val)?;
+            Ok(val)
+        })
+        .collect::<Result<Vec<_>, ApiError>>()?;
+
+    Ok(Json(ingredients))
+}
+
+async fn get_all_ingredients(State(s): State<AppState>) -> Result<Json<Vec<Ingredient>>, ApiError> {
+    let tree = s.db.open_tree("ingredients")?;
+
+    let ingredients = tree
+        .iter()
+        .map(|elem| {
+            let val = elem?.1;
+            let val = bincode::deserialize(&val)?;
+            Ok(val)
+        })
+        .collect::<Result<Vec<_>, ApiError>>()?;
+
+    Ok(Json(ingredients))
 }
